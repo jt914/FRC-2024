c8043c197340dbb3972417ca6673b8803e412147
WillyumP <107447165+WillyumP@users.noreply.github.com>
2024-02-16 16:30:04 -0500

    Commit

---
+++ src/main/java/frc/robot/Commands/AmpCommand.java
@@ -0,0 +1,50 @@
+package frc.robot.Commands;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
+import frc.robot.Subsystems.Arm;
+import frc.robot.Subsystems.Intake;
+import frc.robot.Subsystems.Shooter;
+
+public class AmpCommand extends Command {
+    private Arm arm;
+    private Intake intake;
+    private Shooter shooter;
+    private boolean isFinished = false;
+    private boolean forward;
+
+    public AmpCommand(){
+
+    }
+
+
+    @Override
+    public void initialize(){
+        arm = Constants.arm;
+        intake = Constants.intake;
+        shooter = Constants.shooter;
+    }
+
+    @Override
+    public void execute(){
+        arm.setDesired(100);
+        arm.moveArm();
+        shooter.setSpeed(-.4, -0.55);
+        if(shooter.getSpeedBottom() > 2000 && arm.armEnc.getDistance() > 95) {
+            intake.run(.5);
+        }
+        
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+
+
+
+
+
+
+}
+
