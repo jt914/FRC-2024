8bf123943fe34523be0e7ad70d99b4bcaefbe933
WillyumP <107447165+WillyumP@users.noreply.github.com>
2024-02-21 15:09:59 -0500

    Unfinished

--- src/main/java/frc/robot/Subsystems/Arm.java
+++ src/main/java/frc/robot/Subsystems/Arm.java
@@ -12,15 +12,18 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.ProfiledPIDSubsystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 
-public class Arm extends SubsystemBase{
+public class Arm extends ProfiledPIDSubsystem{
 
     public double currentPos;
     private CANSparkMax armLeft,armRight;
@@ -33,8 +36,20 @@ public class Arm extends SubsystemBase{
     // public PIDController controller = new PIDController(.2, .13, 0);
     private final ArmFeedforward feedforward  = new ArmFeedforward(0, .45, .5);
     public DigitalInput armSwitch;
-
+    private static double maxVelocity = 90; // degrees per second
+    private static double maxAcceleration = 200; // degrees per seconds^2
+    private static ProfiledPIDController profiledPIDController = new ProfiledPIDController(
+            .1,
+            .003,
+            .01,
+            new TrapezoidProfile.Constraints(
+                maxVelocity, // degrees per second
+                maxAcceleration) // degrees per second^2
+                , 0.02);
+                
     public Arm() {
+        super(profiledPIDController);
+
         armLeft = new CANSparkMax(Constants.armLeftID, MotorType.kBrushless);
         armLeft.restoreFactoryDefaults();
         armLeft.setIdleMode(IdleMode.kBrake);
@@ -69,9 +84,6 @@ public class Arm extends SubsystemBase{
 
         // armEnc.reset();
 
-        kP = .05;
-        kI = 0;
-        kD = 0;
     }
 
     public void stall(){
@@ -92,59 +104,40 @@ public class Arm extends SubsystemBase{
     public void climbUp(){
     }
 
-    public void setDesired(double desired){
-        desiredAngle = desired;
-    }
 
-    public void moveArm() {
-        SmartDashboard.putNumber("Brake", armLeft.getBusVoltage()*armLeft.getAppliedOutput());
+
         
-        double k = Math.signum(desiredAngle - armEnc.getDistance());
-        if(Math.abs(desiredAngle-armEnc.getDistance()) < 2)
+        
+    
+    public double getMeasurement() {
+        return armEnc.getDistance();
+        
+    }
+    public void useOutput(double output, TrapezoidProfile.State desiredSetpoint) {
+        TrapezoidProfile.Constraints constraints = new TrapezoidProfile.Constraints(
+            maxVelocity, // degrees per second
+            maxAcceleration); // degrees per second^2
+        
+        profiledPIDController.setConstraints(constraints);
+        double k = Math.signum(output - armEnc.getDistance());
+        if(Math.abs(output-armEnc.getDistance()) < 2)
         {
             k = 0;
         }
-        SmartDashboard.putNumber("pid", controller.calculate(armEnc.getDistance(),desiredAngle) );
-        SmartDashboard.putNumber("ff", feedforward.calculate(desiredAngle* Math.PI / 180, Math.PI/10));
+        SmartDashboard.putNumber("pid", controller.calculate(armEnc.getDistance(),output) );
+        SmartDashboard.putNumber("ff", feedforward.calculate(output* Math.PI / 180, Math.PI/10));
         SmartDashboard.putNumber("k", k);
-        // armLeft.setVoltage(controller.calculate(armEnc.getDistance(), desiredAngle) + MathUtil.clamp(feedforward.calculate(desiredAngle* Math.PI / 180, k), -2, 2));
-        // armRight.setVoltage(controller.calculate(armEnc.getDistance(), desiredAngle) + MathUtil.clamp(feedforward.calculate(desiredAngle * Math.PI / 180, k), -2, 2));
-        armLeft.setVoltage(controller.calculate(armEnc.getDistance(), desiredAngle) + (feedforward.calculate(armEnc.getDistance()* Math.PI / 180, k * .3)));
-        armRight.setVoltage(controller.calculate(armEnc.getDistance(), desiredAngle) + (feedforward.calculate(armEnc.getDistance() * Math.PI / 180, k * .3)));
-        SmartDashboard.putNumber("Left PID", controller.calculate(armEnc.getDistance(), desiredAngle));
-        SmartDashboard.putNumber("Left FF", MathUtil.clamp(feedforward.calculate(desiredAngle * Math.PI / 180, k), -2, 2));
+        // armLeft.setVoltage(controller.calculate(armEnc.getDistance(), output) + MathUtil.clamp(feedforward.calculate(output* Math.PI / 180, k), -2, 2));
+        // armRight.setVoltage(controller.calculate(armEnc.getDistance(), output) + MathUtil.clamp(feedforward.calculate(output * Math.PI / 180, k), -2, 2));
+        armLeft.setVoltage(controller.calculate(armEnc.getDistance(), output) + (feedforward.calculate(armEnc.getDistance()* Math.PI / 180, k * .3)));
+        armRight.setVoltage(controller.calculate(armEnc.getDistance(), output) + (feedforward.calculate(armEnc.getDistance() * Math.PI / 180, k * .3)));
+        SmartDashboard.putNumber("Left PID", controller.calculate(armEnc.getDistance(), output));
+        SmartDashboard.putNumber("Left FF", MathUtil.clamp(feedforward.calculate(output * Math.PI / 180, k), -2, 2));
         SmartDashboard.putNumber("p", controller.getPositionError() * controller.getP());
         SmartDashboard.putNumber("i", controller.getIZone()* controller.getI());
         SmartDashboard.putNumber("d", controller.getVelocityError()* controller.getD());
         SmartDashboard.putData("pidControl", controller);
     }
-    public double getArmEncoder() {
-        return armEnc.getDistance();
-    }
-
-
-
-
-
-
-        // if(forward){
-        //     speed = - 1 * kP * Math.abs(currentPos - desiredAngle); //speed is from -1.0 to 1.0
-        // }
-        // else{
-        //     speed = 1 * kP * Math.abs(desiredAngle - currentPos); //speed is from -1.0 to 1.0
-        // }
-
-        // if(speed > 0.3){
-        //     return;
-        // }
-        
-        
-        // armLeft.set(speed);
-        // armRight.set(speed);
-
-        // SmartDashboard.putNumber("speed", speed);
-
-    
 
     public void stop() {
         armLeft.set(0);
