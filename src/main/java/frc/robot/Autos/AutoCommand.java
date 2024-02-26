54a8b957a601040cd6525900e0243d8c00bdbeb4
jt914 <josephtso914@gmail.com>
2024-02-26 09:16:05 -0500

    working

--- src/main/java/frc/robot/Autos/AutoCommand.java
+++ src/main/java/frc/robot/Autos/AutoCommand.java
@@ -23,13 +23,23 @@ public class AutoCommand extends Command {
     private int intakeStep;
     private int armStep;
     private Transform3d target;
-    private PIDController aimController = new PIDController(.002, 0.000001, 0);
+    private PIDController turnPID = new PIDController(.002, 0.000001, 0);
     private PIDController drivePID = new PIDController(1.5, 0.0001, 0);
-    private int counter;
+    private int counter, shooterCounter;
     private int step = 1;
+    private double xSpeed, ySpeed, yaw;
+    private Shooter shooter;
+    private Arm arm;
+    private Intake intake;
 
     public AutoCommand(){
+        addRequirements(swerve, shooter, arm, intake);
         swerve = Constants.swerve;
+        shooter = Constants.shooter;
+        arm = Constants.arm;
+        intake = Constants.intake;
+
+
     }
 
     @Override
@@ -37,40 +47,142 @@ public class AutoCommand extends Command {
 
     }
 
+    //things to do: Figure out if gyro is rotating which direction
+    //Find exact locations of the notes
+    //Tune PID and Drive PID
+
+
     @Override
     public void execute(){
-        if(target != null){
-            double xSpeed = drivePID.calculate(target.getX(), 1);
-            double ySpeed = drivePID.calculate(target.getY(), -4);
-
-            SmartDashboard.putNumber("xCurrent", target.getX());
-            SmartDashboard.putNumber("yCurrent", target.getY());
-
-            SmartDashboard.putNumber("xCalculated", xSpeed);
-            SmartDashboard.putNumber("yCalculated", ySpeed);
-        }
-
         target = Constants.camera.getTarget();
 
-
-        if(step == 1 && target != null){
+        if(target != null){
             counter = 0;
-            double xSpeed = drivePID.calculate(target.getX(), 2.5);
-            double ySpeed = drivePID.calculate(target.getY(), -0.06);
-            if(target.getX() - 2.5 < 0.1 && target.getY() < 0.1){
-                step = 2;
-                System.out.println("step2");
+            SmartDashboard.putNumber("xCurrent", target.getX());
+            SmartDashboard.putNumber("yCurrent", target.getY());
+            SmartDashboard.putNumber("Step", step);
+            
+            //shoot
+            if(step == 1){
+                shooter.setLowVelocity();
+                shooterCounter++;
+                if(shooterCounter > 10){
+                    shooterCounter = 0;
+                    step = 2;
+                    shooter.stop();
+                }
+
             }
-            swerve.drive(xSpeed, ySpeed, 0);
+            //drive and intake 1st note
+            if(step == 2){
+                xSpeed = drivePID.calculate(target.getX(), 2.65);
+                ySpeed = drivePID.calculate(target.getY(), 1.03);
+                yaw = -1 * turnPID.calculate(-120, Constants.m_gyro.getTotalAngleDegrees());
+                yaw = 0;
+                swerve.drive(xSpeed, ySpeed, yaw);
+                intake.run();
+
+                if(Math.abs(target.getX()) - 2.65 < 0.1 && Math.abs(target.getY()) - 1.03 < 0.1){
+                    step = 3;
+                    intake.stop();
+                }
+
+            }
+
+            //drive to center
+            if(step == 3){
+                if(Math.abs(target.getX()) - 2 < 0.1 && Math.abs(target.getY() - 0) < 0.1){
+                    step = 4;
+                }
+                xSpeed = drivePID.calculate(target.getX(), 2);
+                ySpeed = drivePID.calculate(target.getY(), 0);
+                yaw = -1 * turnPID.calculate(-120, Constants.m_gyro.getTotalAngleDegrees());
+
+                //yaw = aimController.calculate(-1 + desired[0], 0);
+                yaw = 0;
+                swerve.drive(xSpeed, ySpeed, yaw);
+            }
+
+            //shoot
+            if(step == 4){
+                shooter.setLowVelocity();
+                shooterCounter++;
+                if(shooterCounter > 10){
+                    shooterCounter = 0;
+                    step = 5;
+                    shooter.stop();
+                }
+            }
+
+            //drive to intake 2nd note
+            if(step == 5){
+                xSpeed = drivePID.calculate(target.getX(), 2.7);
+                ySpeed = drivePID.calculate(target.getY(), 0);
+                yaw = -1 * turnPID.calculate(-120, Constants.m_gyro.getTotalAngleDegrees());
+                yaw = 0;
+                swerve.drive(xSpeed, ySpeed, yaw);
+                intake.run();
+
+                if(Math.abs(target.getX()) - 2.7 < 0.1 && Math.abs(target.getY()) - 0 < 0.1){
+                    step = 6;
+                    intake.stop();
+                }
+
+            }
+
+            //shoot
+
+            if(step == 6){
+                shooter.setLowVelocity();
+                shooterCounter++;
+                if(shooterCounter > 10){
+                    shooterCounter = 0;
+                    step = 7;
+                    shooter.stop();
+                }
+            }
+
+            //drive to next location
+            if(step == 7){
+                if(Math.abs(target.getX()) - 2 < 0.1 && Math.abs(target.getY()) - 1 < 0.1){
+                    step = 8;
+                }
+                xSpeed = drivePID.calculate(target.getX(), 2);
+                ySpeed = drivePID.calculate(target.getY(), -1.03);
+                yaw = -1 * turnPID.calculate(-120, Constants.m_gyro.getTotalAngleDegrees());
+                yaw = 0;
+                swerve.drive(xSpeed, ySpeed, yaw);
+            }
+
+            //intake note
+            if(step == 8){
+                xSpeed = drivePID.calculate(target.getX(), 2.65);
+                ySpeed = drivePID.calculate(target.getY(), -1.03);
+                yaw = -1 * turnPID.calculate(-120, Constants.m_gyro.getTotalAngleDegrees());
+                yaw = 0;
+                swerve.drive(xSpeed, ySpeed, yaw);
+                intake.run();
+                if(target.getX() - 2.65 < 0.1 && Math.abs(target.getY()) -1 < 0.1){
+                    intake.stop();
+                    step = 9;
+                }
+
+                
+            }
+
+            //shoot
+            if(step == 9){
+                shooter.setLowVelocity();
+                shooterCounter++;
+                if(shooterCounter > 10){
+                    shooterCounter = 0;
+                    step = 10;
+                    shooter.stop();
+                }
+            }
         }
 
-        if(step == 2){
-            if(Constants.m_gyro.getTotalAngleDegrees() < -175){
-                step = 3;
-            }
-            swerve.drive(0,0,aimController.calculate(Constants.m_gyro.getTotalAngleDegrees(), -180));
-
-        }
+        
         if(target == null){
             counter++;
         }
